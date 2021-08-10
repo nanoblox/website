7d1ef348a81f500731c68fdca9f62a9814b0a4d0
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-08-10 15:13:32 -0400

    Updated yml

---
+++ src/resources.js
@@ -0,0 +1,12 @@
+(() => {
+  const resources = db.getCollection("resources");
+
+  const result = resources.insertOne({
+    imageUrl: "ImageUrl",
+    title: "Title",
+    description: "Description",
+    resourceUrl: "ResourceUrl",
+  });
+
+  printjson(result);
+})();
